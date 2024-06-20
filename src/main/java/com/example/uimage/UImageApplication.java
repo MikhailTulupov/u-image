49f7a81dfbd027e893250772d9309f49f9321b77
MikhailTulupov <kd.maikl-201@yandex.ru>
2024-06-20 13:50:41 +0900

    Init commit

---
+++ src/main/java/com/example/uimage/UImageApplication.java
@@ -0,0 +1,13 @@
+package com.example.uimage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UImageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UImageApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/uimage/UImageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.uimage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UImageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
